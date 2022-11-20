7a10782c83f35719833b0209c98b0bbf7c7faa1a
Julian Elmasry <julian31186@gmail.com>
2022-11-19 22:58:57 -0500

    todo list

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,6 +2,12 @@ import { Client } from "discord.js";
 import ready from "./listeners/ready";
 import interactionCreate from "./listeners/interactionCreate";
 
+//TODO
+//port token to env b4 going public
+//figure out COMPLETELY how this discordjs lib is working and how everything works in conjunction
+//figure out how to set up MONGODB and setup a user
+//setup prisma
+
 
 //PORT TO ENV BEFORE HOSTING BOT
 const token = "MTA0MzY3NjgzMDkwNjk4MjQ2MA.GkBSiW.Z34CgSRNBJ62io-6_RjOPCBFD9JBULgehORcVA"
